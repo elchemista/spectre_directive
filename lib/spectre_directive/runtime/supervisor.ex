33529228457aa5c4cf19a67bf553686019001266
Yuriy Zhar <darkyfelis@gmail.com>
2026-07-23 01:15:48 +0200

    refactor: supervise Directive runtime components

--- lib/spectre_directive/runtime/supervisor.ex
+++ lib/spectre_directive/runtime/supervisor.ex
@@ -1,52 +1,34 @@
 defmodule SpectreDirective.Runtime.Supervisor do
-  @moduledoc """
-  Optional supervision tree for mission runtime infrastructure.
-
-  Host applications can supervise this module explicitly. Script-style callers
-  can also use `SpectreDirective.start_mission/2` directly; the runtime will be
-  started lazily when no host supervisor is present.
-  """
+  @moduledoc "Optional supervision tree for locally executed mission loops."
 
   use Supervisor
 
   @registry SpectreDirective.Registry
   @mission_supervisor SpectreDirective.MissionSupervisor
-  @supervisor SpectreDirective.Supervisor
+  @task_supervisor SpectreDirective.TaskSupervisor
 
-  @doc """
-  Starts the runtime supervisor.
-  """
+  @doc "Starts the Registry, mission supervisor, and callback task supervisor."
   @spec start_link(keyword()) :: Supervisor.on_start()
-  def start_link(opts \\ []) when is_list(opts) do
-    Supervisor.start_link(__MODULE__, opts, name: Keyword.get(opts, :name, @supervisor))
+  def start_link(opts \\ []) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  @doc false
-  @spec child_spec(keyword()) :: Supervisor.child_spec()
-  def child_spec(opts) when is_list(opts) do
-    %{
-      id: Keyword.get(opts, :id, __MODULE__),
-      start: {__MODULE__, :start_link, [opts]},
-      type: :supervisor
-    }
-  end
-
-  @doc """
-  Ensures the runtime registry and dynamic supervisor are available.
-  """
+  @doc "Starts the runtime lazily when the host did not add it to a supervision tree."
   @spec ensure_started(keyword()) :: :ok | {:error, term()}
-  def ensure_started(opts \\ []) when is_list(opts) do
+  def ensure_started(opts \\ []) do
     case Process.whereis(@mission_supervisor) do
       pid when is_pid(pid) -> :ok
       nil -> start_runtime(opts)
     end
   end
 
+  @doc false
+  @spec init(keyword()) :: {:ok, {Supervisor.sup_flags(), [Supervisor.child_spec()]}} | :ignore
   @impl Supervisor
-  @spec init(keyword()) :: {:ok, {Supervisor.sup_flags(), [Supervisor.child_spec()]}}
   def init(_opts) do
     children = [
       {Registry, keys: :unique, name: @registry},
+      {Task.Supervisor, name: @task_supervisor},
       {DynamicSupervisor, strategy: :one_for_one, name: @mission_supervisor}
     ]
 
